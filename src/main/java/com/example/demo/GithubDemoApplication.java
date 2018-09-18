a35f434281a260ed41efba2ac60c3fd6b5ebb8aa
zhongxingduo <zhongxingduo@163.com>
2018-09-18 22:48:29 +0800

    test

--- src/main/java/com/example/demo/GithubDemoApplication.java
+++ src/main/java/com/example/demo/GithubDemoApplication.java
@@ -11,7 +11,7 @@ public class GithubDemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!!!";
+        return "Hello World!!!@@@";
     }
 
     public static void main(String[] args) {
